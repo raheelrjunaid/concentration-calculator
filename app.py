79a7624019a8118b8814eda2b96d4b5f0d5da141
Raheel Junaid <60077374+raheelrjunaid@users.noreply.github.com>
2020-11-06 21:27:46 -0700

    Update app.py
    Add molar mass for calculation
    Add return statement with conversions and good appearance.

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
-def convertUp(value):
-    value = value * 1000
-    return value
-
-def convertDown(value):
-    value = value / 1000
-    return value
-
-def replaceLetter(str, letter):
-    str = str.replace(letter, "")
-    return str
+from func import *
 
 # %w/v = g/ml
 # ppm = mg/kg or mg/L
 # M = mol/L
 
-while True:
-    mass = (input("Input total mass of compound: "))
+while True: # Validation
+    mass = input("Input total mass of compound: ")
     if mass.lower().endswith("g"):
         if mass.lower()[-2:] == "mg":
             mass = convertDown(float(replaceLetter(mass, "mg")))
@@ -29,8 +19,8 @@ while True:
     else:
         print("Mass must be in g, mg or kg")
 
-while True:
-    volume = (input("Input total volume of compound: "))
+while True: # Validation
+    volume = input("Input total volume of compound: ")
     if volume.lower().endswith("l"):
         if volume.lower()[-2:] == "ml":
             volume = convertDown(float(replaceLetter(volume, "ml")))
@@ -44,17 +34,42 @@ while True:
     else:
         print("Volume must be in l, ml or kl")
 
+while True: # Validation
+    try:
+        molarMass = float(input("Input compound molar mass [g/mol]: "))
+        break
+    except ValueError:
+        print("Molar Mass has to be only a number.")
+
 firstElement = input("Input first element in compound (no sub/superscript): ")
-while True:
+while True: # Validation
     try:
-        firstMolAmount = int(input("Input first element molar quantity (2 for 2Pb):"))
+        firstMolAmount = int(input("Input first element molar quantity (2 for 2Pb): "))
         break
     except ValueError:
         print("Molar quantity has to be a number.")
+        
 secondElement = input("Input second element in compound (no sub/superscript): ")
-while True:
+while True: # Validation
     try:
-        secondMolAmount = int(input("Input second element molar quantity (3 for 3Al):"))
+        secondMolAmount = int(input("Input second element molar quantity (3 for 3Al): "))
         break
     except ValueError:
         print("Molar quantity has to be a number.")
+
+# Weight to Mass Conversions
+w_v = mass / convertUp(volume)
+w_v = format(w_v, ".2%")
+ppm = convertUp(mass) / volume
+
+# Molar Conversions
+mol = mass / molarMass
+mol_L = mol / volume
+firstElementConc = firstMolAmount * mol_L
+secondElementConc = secondMolAmount * mol_L
+
+# Appearance
+br = "\n| " + "-" * 35 + "\n| "
+
+# Return
+print(f"\nResults:{br}Percentage Concentration: {w_v}{br}Parts Per Million: {sciNotation(ppm)} ppm {br}Molar Concentration: {sciNotation(mol_L)} mol/L {br}Molar Concentration of {firstElement} [{firstElement}] = {sciNotation(firstElementConc)} mol/L {br}Molar Concentration of {secondElement} [{secondElement}] = {sciNotation(secondElementConc)} mol/L\n")
